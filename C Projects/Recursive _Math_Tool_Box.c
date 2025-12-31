ef48d10815882a241c15eccbeaeb517c35d12026
imrajeevraj <rajeev.ranjan948@hotmail.com>
2026-01-01 01:34:23 +0530

    Update Recursive _Math_Tool_Box.c

--- C Projects/Recursive _Math_Tool_Box.c
+++ C Projects/Recursive _Math_Tool_Box.c
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <stdbool.h>
-
-#define PI 3.14159265358979323846
 
 // Function prototypes
 double add(double a, double b);
@@ -15,11 +12,6 @@ double multiply(double a, double b);
 double divide(double a, double b);
 double power(double base, int exponent);     
 double factorial(int n);                  
-double sine(double degrees);
-double cosine(double degrees);
-double tangent(double degrees);
-double logarithm(double value);
-double natural_logarithm(double value);
 double square_root(double value);
 void clear_input_buffer();
 
@@ -37,13 +29,8 @@ int main() {
         printf("4. Division\n");
         printf("5. Power (Recursive)\n");
         printf("6. Factorial (Recursive)\n");
-        printf("7. Sine\n");
-        printf("8. Cosine\n");
-        printf("9. Tangent\n");
-        printf("10. Logarithm (base 10)\n");
-        printf("11. Natural Logarithm\n");
-        printf("12. Square Root\n");
-        printf("13. Exit\n");
+        printf("7. Square Root\n");
+        printf("8. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
         clear_input_buffer();
@@ -90,44 +77,8 @@ int main() {
                 else
                     printf("Result: %.0lf\n", factorial(int_num));
                 break;
-
+                
             case 7:
-                printf("Enter angle in degrees: ");
-                scanf("%lf", &num1);
-                printf("Result: %.6lf\n", sine(num1));
-                break;
-
-            case 8:
-                printf("Enter angle in degrees: ");
-                scanf("%lf", &num1);
-                printf("Result: %.6lf\n", cosine(num1));
-                break;
-
-            case 9:
-                printf("Enter angle in degrees: ");
-                scanf("%lf", &num1);
-                printf("Result: %.6lf\n", tangent(num1));
-                break;
-
-            case 10:
-                printf("Enter positive number: ");
-                scanf("%lf", &num1);
-                if (num1 <= 0)
-                    printf("Error: Invalid input!\n");
-                else
-                    printf("Result: %.6lf\n", logarithm(num1));
-                break;
-
-            case 11:
-                printf("Enter positive number: ");
-                scanf("%lf", &num1);
-                if (num1 <= 0)
-                    printf("Error: Invalid input!\n");
-                else
-                    printf("Result: %.6lf\n", natural_logarithm(num1));
-                break;
-
-            case 12:
                 printf("Enter non-negative number: ");
                 scanf("%lf", &num1);
                 if (num1 < 0)
@@ -136,7 +87,7 @@ int main() {
                     printf("Result: %.6lf\n", square_root(num1));
                 break;
 
-            case 13:
+            case 8:
                 printf("Exiting... Goodbye!\n");
                 exit(0);
 
@@ -167,27 +118,7 @@ double factorial(int n) {
         return 1;
     return n * factorial(n - 1);
 }
-
-double sine(double degrees) {
-    return sin(degrees * PI / 180);
-}
-
-double cosine(double degrees) {
-    return cos(degrees * PI / 180);
-}
-
-double tangent(double degrees) {
-    return tan(degrees * PI / 180);
-}
-
-double logarithm(double value) {
-    return log10(value);
-}
-
-double natural_logarithm(double value) {
-    return log(value);
-}
-
+// Square root function
 double square_root(double value) {
     return sqrt(value);
 }

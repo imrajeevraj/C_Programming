42ca92b1bd5019d637a31250ab8592e26143f862
imrajeevraj <rajeev.ranjan948@hotmail.com>
2026-01-01 17:29:32 +0530

    Update PA5_C (3).c

--- Practical Assignment/PA5_C (3).c
+++ Practical Assignment/PA5_C (3).c
@@ -1,1 +1,21 @@
+// C Program to Count the Number of Words in a String
+
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i, count = 1;
+
+    printf("Enter a sentence: ");
+    gets(str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' && str[i + 1] != ' ')
+            count++;
+    }
+
+    printf("Number of words = %d\n", count);
+
+    return 0;
+}
 

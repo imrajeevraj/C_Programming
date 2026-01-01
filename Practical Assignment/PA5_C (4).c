0e55044763e40b6bdfe5a0d2a64a93043be61e94
imrajeevraj <rajeev.ranjan948@hotmail.com>
2026-01-01 17:33:09 +0530

    Update PA5_C (4).c

--- Practical Assignment/PA5_C (4).c
+++ Practical Assignment/PA5_C (4).c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 
 int main() {
-    int a[10][10], b[10][10], sum[10][10];
     int r, c, i, j;
 
     printf("Enter number of rows and columns: ");
     scanf("%d %d", &r, &c);
-
+    
+    int a[r][c], b[r][c], sum[r][c];
+    
     printf("Enter elements of first matrix:\n");
     for (i = 0; i < r; i++) {
         for (j = 0; j < c; j++) {
